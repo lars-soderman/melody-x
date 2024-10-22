b4c780933a16c080dc7e142c50e2a1e2ec0e7743
Lars Söderman <lars.soderman@valtech.com>
2024-10-22 20:30:09 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 "use client";
-
 import { useState } from "react";
 
 type Box = {
-  id: string;
   letter: string | null;
-  // onClick: () => void;
   row: number;
   col: number;
 };
 
-const Box = ({ letter, onClick, id }: Box) => {
+type ShowBoxProps = Box & {
+  onClick: () => void;
+  id: string;
+};
+
+const ShowBox = ({ letter, onClick, id }: ShowBoxProps) => {
   return (
     <button
       key={id}
@@ -22,192 +24,201 @@ const Box = ({ letter, onClick, id }: Box) => {
   );
 };
 
-const existingIds = new Set<string>();
-
-const createBox = (letter: string | null) => {
-  let id;
-  do {
-    id = generateGUID();
-  } while (existingIds.has(id));
-  existingIds.add(id);
-
-  return {
-    letter,
-    onClick: () => {},
-    id,
-  };
+const getId = (row: number, col: number) => {
+  return `box-row-${row}-col-${col}`;
 };
 
-const generateGUID = () => {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0,
-      v = c === "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
+const initialBoxes: Box[] = [
+  {
+    letter: null,
+    row: 0,
+    col: 0,
+  },
+  {
+    letter: null,
+    row: 0,
+    col: 1,
+  },
+  {
+    letter: null,
+    row: 1,
+    col: 0,
+  },
+  {
+    letter: null,
+    row: 1,
+    col: 1,
+  },
+];
+
+// Utility to get minimum and maximum rows and columns
+const getMaxRow = (boxes: Box[]) => Math.max(...boxes.map((box) => box.row));
+const getMaxCol = (boxes: Box[]) => Math.max(...boxes.map((box) => box.col));
+const getMinRow = (boxes: Box[]) => Math.min(...boxes.map((box) => box.row));
+const getMinCol = (boxes: Box[]) => Math.min(...boxes.map((box) => box.col));
+
+const stepsBetween = (num1: number, num2: number) => {
+  return Math.abs(num1 - num2) + 1;
+};
+
+const toGrid = (
+  boxes: Box[],
+  minRow: number,
+  maxRow: number,
+  minCol: number,
+  maxCol: number
+): Box[][] => {
+  const rowCount = stepsBetween(maxRow, minRow);
+  const colCount = stepsBetween(maxCol, minCol);
+
+  return Array.from({ length: rowCount }, (_, rowIndex) => {
+    const row = minRow + rowIndex;
+    return Array.from({ length: colCount }, (_, colIndex) => {
+      const col = minCol + colIndex;
+      return (
+        boxes.find((b) => b.row === row && b.col === col) || {
+          letter: null,
+          row,
+          col,
+        }
+      );
+    });
   });
 };
 
-const initialBox: Box = {
-  letter: null,
-  onClick: () => {},
-  id: generateGUID(),
+type BoxInputProps = {
+  letter: string | null;
+  id: string;
+  onChange: (id: string, letter: string) => void;
 };
 
-type Grid = Box[][];
+const BoxInput = ({ letter, id, onChange }: BoxInputProps) => {
+  return (
+    <input
+      autoFocus
+      key={id}
+      onFocus={(e) => e.target.select()} // Automatically select the text on focus
+      onChange={(e) => onChange(id, e.target.value)}
+      className="border-2 border-black w-16 h-16 text-black text-center flex items-center justify-center text-3xl"
+      value={letter ?? ""}
+    />
+  );
+};
 
-const gridSize = (grid: Grid) => ({
-  rows: grid.length,
-  columns: grid[0].length,
-});
-
-const getGridColumnsClass = (columns: number) => {
-  return `grid-cols-${columns}`;
+const getGridColumnsClass = (numColumns: number) => {
+  return `grid-cols-${numColumns}`;
 };
 
 export default function Home() {
-  const [grid, setGrid] = useState<Grid>([[initialBox]]);
+  const [boxes, setBoxes] = useState<Box[]>(initialBoxes);
   const [editingBox, setEditingBox] = useState<Box | null>(null);
 
-  const addRow = () => {
-    const newRow = Array.from({ length: gridSize(grid).columns }, () =>
-      createBox(null)
+  const minRow = getMinRow(boxes);
+  const maxRow = getMaxRow(boxes);
+  const minCol = getMinCol(boxes);
+  const maxCol = getMaxCol(boxes);
+
+  const grid = toGrid(boxes, minRow, maxRow, minCol, maxCol);
+
+  const addRow = (position: "top" | "bottom") => {
+    const newRowIndex = position === "top" ? minRow - 1 : maxRow + 1;
+    console.log("newRowIndex", newRowIndex);
+    const newRow = Array.from(
+      { length: stepsBetween(maxCol, minCol) },
+      (_, colIndex) => ({
+        letter: null,
+        row: newRowIndex,
+        col: colIndex,
+      })
     );
-    setGrid([...grid, newRow]); // Add the new row at the bottom
+    setBoxes((prevBoxes) => [...prevBoxes, ...newRow]);
   };
 
-  const addRowTop = () => {
-    const newRow = Array.from({ length: gridSize(grid).columns }, () =>
-      createBox(null)
+  const addColumn = (position: "left" | "right") => {
+    const newColIndex = position === "left" ? minCol - 1 : maxCol + 1;
+    const newCol = Array.from(
+      { length: stepsBetween(maxRow, minRow) },
+      (_, rowIndex) => ({
+        letter: null,
+        row: rowIndex,
+        col: newColIndex,
+      })
     );
-    setGrid((prevGrid) => [newRow, ...prevGrid]); // Add the new row at the top
-  };
-
-  const addColumn = () => {
-    setGrid(grid.map((row) => [...row, createBox(null)])); // Add a new box to the right of each row
-  };
-
-  const addColumnLeft = () => {
-    setGrid(grid.map((row) => [createBox(null), ...row])); // Add a new box to the left of each row
+    setBoxes((prevBoxes) => [...prevBoxes, ...newCol]);
   };
 
   const updateBoxLetter = (id: string, newLetter: string) => {
-    setGrid((prevGrid) => {
-      const newGrid = [...prevGrid];
-      const boxToUpdate = newGrid.flat().find((box) => box?.id === id);
-      if (boxToUpdate) {
-        boxToUpdate.letter = newLetter.toUpperCase(); // Convert to uppercase before saving
-      }
-      return newGrid;
-    });
+    setBoxes((prevBoxes) =>
+      prevBoxes.map((box) =>
+        getId(box.row, box.col) === id
+          ? { ...box, letter: newLetter.toUpperCase() }
+          : box
+      )
+    );
     setEditingBox(null);
   };
-
-  type BoxInputProps = { letter: string | null; id: string };
-
-  const BoxInput = ({ letter, id }: BoxInputProps) => {
-    return (
-      <input
-        autoFocus
-        key={id}
-        onFocus={(e) => e.target.select()} // Automatically select the text on focus
-        onChange={(e) => updateBoxLetter(id, e.target.value)}
-        className="border-2 border-black w-16 h-16 text-black text-center flex items-center justify-center text-3xl"
-        value={letter ?? ""}
-      />
-    );
-  };
-
-  const addRowAndColumnTopLeft = () => {
-    addRowTop(); // Add a row at the top
-    addColumnLeft(); // Add a column to the left
-  };
-
-  const addRowAndColumnTopRight = () => {
-    addRowTop(); // Add a row at the top
-    addColumn(); // Add a column to the right
-  };
-
-  const addRowAndColumnBottomLeft = () => {
-    addRow(); // Add a row at the bottom
-    addColumnLeft(); // Add a column to the left
-  };
-
-  const addRowAndColumnBottomRight = () => {
-    addRow(); // Add a row at the bottom
-    addColumn(); // Add a column to the right
-  };
+  console.log("boxes", boxes.length);
+  console.log("grid", grid.flat().length);
 
   return (
     <main className="absolute inset-0 bg-white text-black flex flex-col items-center justify-center">
       <div className="flex relative">
         <div
           className={`grid w-full ${getGridColumnsClass(
-            gridSize(grid).columns
+            maxCol - minCol + 1
           )} gap-0 border-collapse border-2 border-black`}
         >
           {grid.map((row) =>
             row.map((box) =>
-              editingBox?.id === box.id ? (
-                <BoxInput key={box.id} id={box.id} letter={box.letter} />
+              editingBox &&
+              getId(editingBox?.row, editingBox?.col) ===
+                getId(box.row, box.col) ? (
+                <BoxInput
+                  key={getId(box.row, box.col)}
+                  id={getId(box.row, box.col)}
+                  letter={box.letter}
+                  onChange={updateBoxLetter}
+                />
               ) : (
-                <Box
-                  key={box.id}
-                  id={box.id}
+                // <div className="border-2 border-black w-16 h-16 text-black text-center flex items-center text-4xl justify-center">
+                //   {box.row}
+                //   {box.col}
+                // </div>
+                <ShowBox
+                  key={getId(box.row, box.col)}
+                  id={getId(box.row, box.col)}
                   letter={box.letter}
                   onClick={() => setEditingBox(box)}
+                  row={box.row}
+                  col={box.col}
                 />
               )
             )
           )}
         </div>
 
-        {/* Individual Buttons */}
+        {/* Buttons for adding rows and columns */}
         <button
-          onClick={addRowTop} // Add row at the top
+          onClick={() => addRow("top")}
           className="absolute text-2xl bg-slate-50 -translate-x-1/2 left-1/2 h-10 -top-12 rounded w-full opacity-0 hover:opacity-100 p-2 transition-opacity"
         >
           +
         </button>
         <button
-          onClick={addColumnLeft} // Add column to the left
+          onClick={() => addColumn("left")}
           className="absolute text-2xl bg-slate-50 top-1/2 -translate-y-1/2 w-10 -left-12 rounded h-full opacity-0 hover:opacity-100 p-2 transition-opacity"
         >
           +
         </button>
         <button
-          onClick={addRow} // Add row at the bottom
+          onClick={() => addRow("bottom")}
           className="absolute text-2xl bg-slate-50 -translate-x-1/2 left-1/2 h-10 -bottom-12 rounded w-full opacity-0 hover:opacity-100 p-2 transition-opacity"
         >
           +
         </button>
         <button
-          onClick={addColumn} // Add column to the right
+          onClick={() => addColumn("right")}
           className="absolute text-2xl bg-slate-50 top-1/2 -translate-y-1/2 w-10 -right-12 rounded h-full opacity-0 hover:opacity-100 p-2 transition-opacity"
-        >
-          +
-        </button>
-
-        {/* Corner Buttons */}
-        <button
-          onClick={addRowAndColumnTopLeft} // Add row at the top and column to the left
-          className="absolute text-2xl bg-slate-50 -left-12 h-10 -top-12 rounded w-10 opacity-0 hover:opacity-100 p-2 transition-opacity"
-        >
-          +
-        </button>
-        <button
-          onClick={addRowAndColumnTopRight} // Add row at the top and column to the right
-          className="absolute text-2xl bg-slate-50 -right-12 h-10 -top-12 rounded w-10 opacity-0 hover:opacity-100 p-2 transition-opacity"
-        >
-          +
-        </button>
-        <button
-          onClick={addRowAndColumnBottomLeft} // Add row at the bottom and column to the left
-          className="absolute text-2xl bg-slate-50 -left-12 h-10 -bottom-12 rounded w-10 opacity-0 hover:opacity-100 p-2 transition-opacity"
-        >
-          +
-        </button>
-        <button
-          onClick={addRowAndColumnBottomRight} // Add row at the bottom and column to the right
-          className="absolute text-2xl bg-slate-50 -right-12 h-10 -bottom-12 rounded w-10 opacity-0 hover:opacity-100 p-2 transition-opacity"
         >
           +
         </button>
