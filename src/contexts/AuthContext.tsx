80773e53bbfe3cb92bdc77387bb261b4b9da88bb
Lars Söderman <lars.soderman@valtech.com>
2024-11-11 21:25:34 +0100

    unpublic

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,7 +3,7 @@ import { createClient, User } from '@supabase/supabase-js';
 import { createContext, useContext, useEffect, useState } from 'react';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
